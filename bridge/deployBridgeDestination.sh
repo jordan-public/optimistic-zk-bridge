625c9390e26c6e077070a36202eefefa66ee0259
jordan <jordan@bitola.com>
2025-05-31 22:26:37 +0200

    Deployment fails with InvalidChainId()

--- bridge/deployBridgeDestination.sh
+++ bridge/deployBridgeDestination.sh
@@ -6,4 +6,4 @@ set -e
 . ./.env
 
 # To deploy and verify our contract
-forge script script/BridgeDestination.s.sol:Deploy --rpc-url "http://127.0.0.1:8546/" --sender $DEPLOYER_ADDRESS --private-key $DEPLOYER_PRIVATE_KEY --broadcast -v
+forge script script/BridgeDestination.s.sol:Deploy --rpc-url "http://127.0.0.1:8546/" --chain-id 31338 --sender $DEPLOYER_ADDRESS --private-key $DEPLOYER_PRIVATE_KEY --broadcast -v
--- bridge/script/BridgeDestination.s.sol
+++ bridge/script/BridgeDestination.s.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {MisbehaviorProver} from "../src/MisbehaviorProver.sol";
 import {BridgeDestination} from "../src/BridgeDestination.sol";
 
 contract Deploy is Script {
@@ -24,14 +25,24 @@ contract Deploy is Script {
             );
         }
 
-        // Print the balanve of the deployer (currently the script runner)
+        // Print the balance of the deployer (currently the script runner)
         console.log("Deployer address:", vm.envAddress("DEPLOYER_ADDRESS"));
         console.log("Deployer balance:", vm.envAddress("DEPLOYER_ADDRESS").balance);
         console.log("Relayer address:", vm.envAddress("RELAYER_ADDRESS"));
         console.log("Relayer balance:", vm.envAddress("RELAYER_ADDRESS").balance);
 
         address relayer = vm.envAddress("RELAYER_ADDRESS");
-        bridgeDestination = new BridgeDestination(relayer, vm.envAddress("SOURCE_ERC20_ADDRESS"), vm.envAddress("DESTINATION_ERC20_ADDRESS"));
+
+        // Deploy the MisbehaviorProver contract
+        MisbehaviorProver misbehaviorProver = new MisbehaviorProver(
+            relayer,
+            vm.envAddress("SOURCE_ERC20_ADDRESS"),
+            vm.envAddress("DESTINATION_ERC20_ADDRESS")
+        );
+        console.log("MisbehaviorProver deployed at:", address(misbehaviorProver));
+
+        // Deploy the BridgeDestination contract which contains the verifier
+        BridgeDestination bridgeDestination = new BridgeDestination(relayer, vm.envAddress("SOURCE_ERC20_ADDRESS"), vm.envAddress("DESTINATION_ERC20_ADDRESS"));
 
         console.log("BridgeDestination deployed at:", address(bridgeDestination));
 
